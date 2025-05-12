a6ccde2eee866553327556bbe5859682e1319982
samarthakara <icodeanonymously@gmail.com>
2025-05-12 10:32:21 +0530

    Update fetch_reddit.py

--- Web_version/Backend/fetch_reddit.py
+++ Web_version/Backend/fetch_reddit.py
@@ -1,22 +1,23 @@
 import asyncpraw
 import os
 from datetime import datetime, timezone
+from dotenv import load_dotenv
+
+load_dotenv()
 
 reddit = asyncpraw.Reddit(
-    client_id=os.getenv("REDDIT_CLIENT_ID"),
-    client_secret=os.getenv("REDDIT_SECRET"),
-    user_agent=os.getenv("REDDIT_USER_AGENT")
+    client_id=os.environ.get('REDDIT_CLIENT_ID'),
+    client_secret=os.environ.get('REDDIT_CLIENT_SECRET'),
+    user_agent=os.environ.get('REDDIT_USER_AGENT')
 )
 
 async def fetch_reddit_data(subreddit_name, limit):
     posts = []
     subreddit = await reddit.subreddit(subreddit_name)
-
     async for post in subreddit.hot(limit=limit):
         posts.append({
             "title": post.title,
             "selftext": post.selftext,
             "created_utc": datetime.fromtimestamp(post.created_utc, timezone.utc).isoformat()
         })
-
     return posts
